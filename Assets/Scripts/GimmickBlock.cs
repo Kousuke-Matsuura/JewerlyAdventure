a0c38c5cad7d02c96fba5626b943fcd1ab26fbe2
Kosuke-Matsuura <matsuura@funleash.co.jp>
2022-07-27 01:14:39 +0900

    7-1ダメージブロックを作ろう、暫定で完了（未知のエラー：[Worker0] Tried select unknown importer for id '-2' '00000000000000000000000000000000'だけが未解決、明日頑張る）

---
+++ Assets/Scripts/GimmickBlock.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickBlock : MonoBehaviour
+{
+    public float length = 0.0f;
+    public bool isDelete = false;
+
+    bool isFell = false;
+    float fadeTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        rbody.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            float d = Vector2.Distance(transform.position, player.transform.position);
+            if(length >= d)
+            {
+                Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+                if(rbody.bodyType == RigidbodyType2D.Static)
+                {
+                    rbody.bodyType = RigidbodyType2D.Dynamic;
+                }
+            }
+        }
+
+        if (isFell)
+        {
+            fadeTime -= Time.deltaTime;
+            Color col = GetComponent<SpriteRenderer>().color;
+            col.a = fadeTime;
+            GetComponent<SpriteRenderer>().color = col;
+            if(fadeTime <= 0.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDelete)
+        {
+            isFell = true;
+        }
+    }
+}
